6a31f81a3100f5a0ebfd63b138c8224aa08637c4
salvinanto7 <salvinanto7@gmail.com>
2021-05-22 14:26:59 +0530

    added remove notes function

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function  createNote(){
+var count=0;
+
+function  createNote(noteTitle,noteBody){
+    count+=1;
+    document.getElementById("no-notes").classList.add("hidden");
 
     let li=document.createElement("li");    
     let a=document.createElement("a");    
     let h3=document.createElement("h3");    
     let p=document.createElement("p");    
-    let deletebtn=document.createElement("deletebtn");    
+    let deletebtn=document.createElement("button");    
 
     deletebtn.classList.add("deletebtn");
 
@@ -24,16 +28,27 @@ function  createNote(){
 
     document.getElementById("notes").appendChild(li);
 
-
 }
 
-
+function removeNote(e){
+    if(e.target.classList.contains("deletebtn")){
+        if(confirm("Are you sure you want to delete the note ?")){
+            let li=e.target.parentElement.parentElement;
+            let ul=document.getElementById("notes");
+            ul.removeChild(li);
+        }
+    }
+    count-=1;
+    if (count<1){
+        document.getElementById("no-notes").className="";
+    }
+}
 function createNoteFromInput(e){
     e.preventDefault();
 
     let noteTitle=document.getElementById("new-note-title-input").value;
     let noteBody=document.getElementById("new-note-content-input").value;
-    
+    console.log(noteTitle,noteBody);
     createNote(noteTitle,noteBody);
 
     document.getElementById("new-note-title-input").value="";
@@ -45,4 +60,5 @@ document
     .getElementById("inputform")
     .addEventListener("submit",createNoteFromInput,false);
 
+document.getElementById("notes").addEventListener("click",removeNote);
     
